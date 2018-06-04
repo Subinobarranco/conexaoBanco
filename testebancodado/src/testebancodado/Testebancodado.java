c7bde3999aee99e69dcbf9d275ea6bdcd799c118
Administrador <Administrador@10.10.4.22>
2018-06-04 20:37:53 -0300

    2018-06-04teste

--- testebancodado/src/testebancodado/Testebancodado.java
+++ testebancodado/src/testebancodado/Testebancodado.java
@@ -16,7 +16,7 @@ public class Testebancodado {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("lellou lord");
+        System.out.println("ola mundo doido");
     }
     
 }
